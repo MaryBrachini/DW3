2714c5d6edfcf1fcfaaac9ac074341a2e7d426e7
marybrachini <marybrachini.com>
2023-09-20 10:40:06 -0300

    20/09

--- dw3frontNode/app.js
+++ dw3frontNode/app.js
@@ -1,11 +1,17 @@
+// DEBUG=dw3frontnode:* npm start
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var alunosRouter = require('./routes/rte_alunos');
+var cursosRouter = require('./routes/rte_cursos');
 
 var app = express();
 
@@ -15,12 +21,25 @@ app.set('view engine', 'vash');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); //Necessário para obter os valores das variáveis via POST
 app.use(cookieParser());
+//app.use( bodyParser.urlencoded({ extended: false, }))
+
+app.use(
+  session({
+    secret: "palavrasecreta",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: null },
+  })
+);
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/alunos', alunosRouter);
+app.use('/cursos',  cursosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
